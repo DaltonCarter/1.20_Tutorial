8a89c285108e8cd7f7c25686c642677757f5edc3
Dalton Carter <Dj_Carter_Gottenbos@yahoo.com>
2024-04-04 20:39:47 -0700

    Added custom tags, modified metal detector.

--- src/main/java/com/CartersDev/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/com/CartersDev/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package com.CartersDev.tutorialmod.item.custom;
 
+import com.CartersDev.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -66,6 +67,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.GOLD_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
